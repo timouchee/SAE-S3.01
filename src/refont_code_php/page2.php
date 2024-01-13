9c308bcf4dbd6e5fa2cd52e1f846a590af80eb1f
timouche <timeo971@gmail.com>
2024-01-13 15:50:01 +0100

    code fonctionel mettre a jour doxygene

--- src/refont_code_php/page2.php
+++ src/refont_code_php/page2.php
@@ -41,34 +41,7 @@ class lst_reponse_utilisateur
                 
 
             }
-            
-            /* for ($i = 0; $i < 10; $i++) 
-            {
-                $element = array();
-                foreach($liste_categorie as $cat)
-                {
-                    // Catégorie "musique" ou "sport"
-                    $lst_cat = array($cat, array());
-                    $ensembleEl = array(); // Ensemble pour éviter les doublons
-                    $nbel = rand(1, 5);
-                    for ($j = 0; $j < $nbel; $j++) {
-                        // Sélection aléatoire d'éléments possibles dans la catégorie
-                        $possible_elements = ($cat === "musique") ? $lst_musique_possible : $lst_sport_possible;
-                        $cat_elements = array_diff($possible_elements, $ensembleEl);
-                        $lst_cat[1][] = $cat_elements[array_rand($cat_elements)];
-                        $ensembleEl[] = end($lst_cat[1]); // Ajouter la dernière valeur à l'ensemble
-                    }
-                    $element[] = $lst_cat;
-                }
-
-                // Ajouter l'élément à la liste
-                $this->lst_rep_U[] = $element;
-            }  */
-
-
-
-
-
+          
             // Ajouter l'élément à la liste
             $this->lst_rep_U[] = $element;
         }
@@ -78,7 +51,7 @@ class lst_reponse_utilisateur
 
     public function toString()
     {
-        echo "<br> les reponse utilisateur (il n'y a que musique et sport)";
+        echo "<br> les reponse utilisateur ";
         foreach ($this->lst_rep_U as $element) 
         {
             echo '<ul>';
@@ -87,6 +60,19 @@ class lst_reponse_utilisateur
             }
             echo '</ul>';
         } 
+    }
+
+    public function ajouter1_reponse_en_dur($rep)
+    {
+        $this->lst_rep_U[] = $rep;
+    }
+
+    public function ajouter_liste_reponse_en_dur($lst_rep)
+    {
+        foreach($lst_rep as $une_rep)
+        {
+            $this->ajouter1_reponse_en_dur($une_rep);
+        }
     }
 
 }
@@ -168,6 +154,7 @@ class Liste_poids
 class Reponse_utilisateur_reduite
 {
     public $res;
+    public $element_de_triage;
 
     public function get_lst_rep_u()
     {
@@ -176,6 +163,7 @@ class Reponse_utilisateur_reduite
 
     public function garder_que_elem($lst_rep_U,$elementPrecis)
     {
+        $this->element_de_triage = $elementPrecis; 
         $this->res = $lst_rep_U;
         $compteur_1reponse = -1;
         foreach($this->res as $une_rep)
@@ -207,7 +195,7 @@ class Reponse_utilisateur_reduite
 
     public function toString()
     {
-        echo "<br> les rep U trier <br>";
+        echo "<br> les rep U trier pour : ". $this->element_de_triage." <br>";
         foreach ($this->res as $element) 
         {
             echo '<ul>';
@@ -271,21 +259,22 @@ class Persona
     } 
     
     
-    public function afficher_perso_etape_0()
+    public function afficher_perso_etape_0($affiche = false)
     {
-        echo "liste des reponse utilisateur pour persona : <br>";
+        if($affiche==true){echo "liste des reponse utilisateur pour persona : <br>";}
+        
         $indic_cat = -1;
         foreach ($this->rep as $categorie) 
         {
             $indic_cat ++;
-            echo $categorie[0] . " :\n";
+            if($affiche==true) {echo $categorie[0] . " :\n";}
             
             if (!empty($categorie[1])) 
             {
                 //compteur  poid totale
                 foreach ($categorie[1] as $element) 
                 {
-                    echo "  [\"{$element[0]}\", {$element[1]} , {$element[2]}     ]\n";
+                    if($affiche==true) {echo "  [\"{$element[0]}\", {$element[1]} , {$element[2]}     ]\n";}
                     if(isset($this->lst_poid_totale[$indic_cat]))
                     {
                         $this->lst_poid_totale[$indic_cat] +=$element[1];
@@ -299,20 +288,22 @@ class Persona
             } 
             else 
             {
-                echo "  (Aucun élément)\n";
+                if($affiche==true) {echo "  (Aucun élément)\n";}
             }
             
-            echo "\n <br>";
+            if($affiche==true) {echo "\n <br>";}
         }
-        echo "<br>";
-        echo "<br>";
+        if($affiche==true){echo "<br>";
+        echo "<br>";}
+        
     }
 
-    public function afficher_perso_etape_1()
+    public function afficher_perso_etape_1($affiche = false)
     {
+        if($affiche==true) {
         echo "maitenant avec le calcule des pourcentage arondi a 2 chiffre apres la virgule ";
         //print_r($this->lst_poid_totale);
-        echo "<br>";
+        echo "<br>";}
     
         $compteur_categorie = -1;
         foreach ($this->rep as $categorie) 
@@ -339,25 +330,29 @@ class Persona
         }
 
         //afficher
-        foreach ($this->rep as $categorie) 
+        if($affiche==true)
         {
-            echo $categorie[0] . " :\n";
-            
-            if (!empty($categorie[1])) 
+           foreach ($this->rep as $categorie) 
             {
-                //compteur  poid totale
-                foreach ($categorie[1] as $element) 
+                echo $categorie[0] . " :\n";
+                
+                if (!empty($categorie[1])) 
                 {
-                    echo "  [\"{$element[0]}\", {$element[1]} , {$element[2]}     ]\n";
+                    //compteur  poid totale
+                    foreach ($categorie[1] as $element) 
+                    {
+                        echo "  [\"{$element[0]}\", {$element[1]} , {$element[2]}     ]\n";
+                    }
+                } 
+                else 
+                {
+                    echo "  (Aucun élément)\n";
                 }
-            } 
-            else 
-            {
-                echo "  (Aucun élément)\n";
-            }
-            
-            echo "\n <br>";
-        } 
+                
+                echo "\n <br>";
+            }  
+        }
+        
 
     }
     
@@ -381,31 +376,34 @@ class Persona
         }
     }
 
-    public function afficher_perso_etape_2()
+    public function afficher_perso_etape_2($affiche = false)
     {
-        echo "<br>";
-        echo "<br>";
-        echo "maitenant trier dans l'ordre";
-        echo "<br>";
+        if($affiche==true){
+
+            echo "<br>";
+            echo "<br>";
+            echo "maitenant trier dans l'ordre";
+            echo "<br>";
+        }
     
         foreach ($this->rep as $categorie) 
         {
-            echo $categorie[0] . " :\n";
+            if($affiche==true) {echo $categorie[0] . " :\n";}
             
             if (!empty($categorie[1])) 
             {
                 //compteur  poid totale
                 foreach ($categorie[1] as $element) 
                 {
-                    echo "  [\"{$element[0]}\", {$element[1]} , {$element[2]}     ]\n";
+                    if($affiche==true) {echo "  [\"{$element[0]}\", {$element[1]} , {$element[2]}     ]\n";}
                 }
             } 
             else 
             {
-                echo "  (Aucun élément)\n";
+                if($affiche==true) {echo "  (Aucun élément)\n";}
             }
             
-            echo "\n <br>";
+            if($affiche==true) {echo "\n <br>";}
         } 
     
         $compt_categorie = -1;
@@ -443,7 +441,7 @@ class Persona
         
             echo "<br>";
             echo "<br>";
-            echo "maitenant sans les 50% premier";
+            echo "<strong> maitenant sans les 50% premier </strong>";
             echo "<br>";
         
             foreach ($this->rep as $categorie) 
@@ -596,7 +594,108 @@ $lst_soirees_possible = array
     "Soirees_mousse"
 );
 
+$liste_reponse_U_en_dur = 
+[
+    // Réponse 1
+    [
+        ["musique", ["Rock", "Electro"]],
+        ["sport", ["Tennis", "Tennis_de_table", "Badminton", "Handball"]],
+        ["culture", ["Numerique", "Voyage", "Cinema"]],
+        ["activitees", ["Bowling", "Tournoi_jeux_videos"]],
+        ["restaurants", ["Junk_food", "Fast_food"]],
+        ["soirees", []]
+    ],
 
+    // Réponse 2
+    [
+        ["musique", ["Rap", "Pop"]],
+        ["sport", ["Course_a_pieds", "Athletisme", "Rugby", "Tennis_de_table", "Football"]],
+        ["culture", ["Voyage", "Cinema"]],
+        ["activitees", ["Plage", "Bowling", "Loup_garoup", "Uno"]],
+        ["restaurants", ["Pizzeria", "Fast_food", "Restaurants_traditionnels"]],
+        ["soirees", ["Boite_de_nuit", "Soiree_etudiante", "Bar", "Soiree_jeux_Societe"]]
+    ],
+
+    // Réponse 3
+    [
+        ["musique", ["Rock", "Jazz", "Electro", "Musiques_film", "Musiques_manga"]],
+        ["sport", ["Badminton", "Tennis_de_table", "Handball", "Esport", "Tennis"]],
+        ["culture", ["Cinema", "Musee", "Zoo", "Voyage", "Numerique"]],
+        ["activitees", ["Tournoi_jeux_videos", "Loup_garoup", "Bowling", "Uno", "Plage"]],
+        ["restaurants", ["Restaurants_du_monde", "Restaurants_traditionnels", "Pizzeria", "Sushis", "Restaurants_vegetariens"]],
+        ["soirees", ["Soiree_jeux_Societe", "Soiree_etudiante", "Bar", "Boite_de_nuit"]]
+    ],
+
+    // Réponse 4
+    [
+        ["musique", ["Pop", "Musiques_film", "Rock", "Classique", "Musiques_manga"]],
+        ["sport", ["Badminton", "Football", "Pala"]],
+        ["culture", ["Voyage", "Cinema", "Numerique", "Lecture", "Zoo"]],
+        ["activitees", ["Loup_garou", "Tournoi_jeux_videos", "Uno"]],
+        ["restaurants", ["Sushis", "Fast_food", "Restaurants_du_monde"]],
+        ["soirees", ["Soiree_jeux_Societe", "Soiree_etudiante", "Bar"]]
+    ],
+
+    // Réponse 5
+    [
+        ["musique", ["Rock", "Musiques_film", "Jazz", "Classique", "Musiques_manga"]],
+        ["sport", ["Tennis", "Badminton", "Tennis_de_table", "Esport", "Basket"]],
+        ["culture", ["Numerique", "Cinema", "Musee", "Voyage", "Zoo"]],
+        ["activitees", ["Bowling", "Plage", "Tournoi_jeux_videos", "Uno", "Loup_garoup"]],
+        ["restaurants", ["Restaurants_du_monde", "Restaurants_traditionnels", "Sushis", "Pizzeria", "Fast_food"]],
+        ["soirees", ["Bar", "Soiree_etudiante", "Boite_de_nuit", "Soiree_jeux_Societe"]]
+    ],
+
+    // Réponse 6
+    [
+        ["musique", ["Rock", "Pop", "Musiques_manga"]],
+        ["sport", ["Football", "Basket"]],
+        ["culture", ["Cinema", "Voyage"]],
+        ["activitees", ["Tournoi_jeux_videos", "Bowling", "Plage"]],
+        ["restaurants", ["Fast_food", "Sushis", "Restaurants_vegetariens"]],
+        ["soirees", ["Boite_de_nuit", "Soiree_jeux_Societe"]]
+    ],
+
+    // Réponse 7
+    [
+        ["musique", ["Classique", "Rap", "Musiques_manga", "Musiques_film"]],
+        ["sport", ["Football"]],
+        ["culture", ["Lecture", "Cinema", "Numerique"]],
+        ["activitees", ["Tournoi_jeux_videos", "Uno", "Loup_garoup"]],
+        ["restaurants", ["Restaurants_traditionnels", "Restaurants_du_monde", "Pizzeria", "Fast_food"]],
+        ["soirees", []]
+    ],
+
+    // Réponse 8
+    [
+        ["musique", ["Rap", "Rock", "Musiques_manga", "Musiques_film"]],
+        ["sport", ["Esport", "Rugby", "Pala"]],
+        ["culture", ["Numerique", "Lecture", "Mediatheque", "Cinema"]],
+        ["activitees", ["Tournoi_jeux_videos", "Loup_garoup", "Bowling", "Uno"]],
+        ["restaurants", ["Fast_food", "Pizzeria", "Sushis", "Restaurants_traditionnels"]],
+        ["soirees", []]
+    ],
+
+    // Réponse 9
+    [
+        ["musique", ["Classique", "Musiques_film", "Jazz", "Musiques_manga"]],
+        ["sport", ["Tennis_de_table", "Badminton", "Athletisme", "Basket", "Esport"]],
+        ["culture", ["Numerique", "Cinema", "Lecture"]],
+        ["activitees", ["Tournoi_jeux_videos", "Bowling"]],
+        ["restaurants", ["Restaurants_traditionnels", "Restaurants_du_monde", "Fast_food"]],
+        ["soirees", ["Soiree_jeux_Societe"]]
+    ],
+
+    // Réponse 10
+    [
+        ["musique", ["Classique", "Jazz", "Musiques_manga"]],
+        ["sport", ["Tennis_de_table", "Tennis", "Course_a_pieds", "Esport"]],
+        ["culture", ["Lecture", "Numerique", "Cinema", "Musee"]],
+        ["activitees", ["Tournoi_jeux_videos", "Bowling", "Loup_garoup"]],
+        ["restaurants", ["Fast_food", "Sushis", "Pizzeria", "Restaurants_traditionnels"]],
+        ["soirees", []]
+    ]
+];
 
 $lst_rep_pos = new Reponse();
 //ici on rajoute les liste musique sport etc...
@@ -618,7 +717,8 @@ array_push($liste_liste_cat_elem,$lst_rep_pos->get_lst_elem("activitees"));
 array_push($liste_liste_cat_elem,$lst_rep_pos->get_lst_elem("restaurants"));
 array_push($liste_liste_cat_elem,$lst_rep_pos->get_lst_elem("soirees"));
 
-$lst_rep_U = $lst_rep_utilisateur->remplirListeReponseU($liste_liste_cat_elem);//par defaut 10
+//$lst_rep_U = $lst_rep_utilisateur->remplirListeReponseU($liste_liste_cat_elem);//par defaut 10
+$lst_rep_utilisateur->ajouter_liste_reponse_en_dur($liste_reponse_U_en_dur);//par defaut 10
 $lst_rep_utilisateur->toString();
 
 $lst_pois = new Liste_poids();
@@ -628,20 +728,20 @@ $lst_pois->toString();
 
 $lst_rep_U_restraint = new Reponse_utilisateur_reduite();
 //mettre la boucle sur la moitier des liste poid ici
-$elementPrecis = "Pop";
+$elementPrecis;
 
 //pour le teste c la boucle
 
-/* $compteur_persona = -1;
+$compteur_persona = -1;
 foreach($lst_pois->get_lst_poid() as $elem)
 {
     $compteur_persona ++;
-    if($compteur_persona >= 10)
+    if($compteur_persona >= 3)
     {break;}
     $elementPrecis = $elem[0];
 
     $lst_rep_U_restraint->garder_que_elem($lst_rep_utilisateur->get_lst_rep_u(),$elementPrecis);
-    $lst_rep_U_restraint->toString();
+    //$lst_rep_U_restraint->toString();
     //$lst_rep_pos->toString(); 
     
     $persona_sans_chiffre = new Persona();
@@ -652,37 +752,24 @@ foreach($lst_pois->get_lst_poid() as $elem)
     $persona_sans_chiffre->afficher_perso_etape_2();
     $persona_sans_chiffre->afficher_perso_etape_3();
     
-
 }    
+ 
+
+
+/* $lst_rep_U_restraint->garder_que_elem($lst_rep_utilisateur->get_lst_rep_u(),$elementPrecis);
+$lst_rep_U_restraint->toString();
+
+$persona_sans_chiffre = new Persona();
+$persona_pas_calculer = $persona_sans_chiffre->persona_creer($lst_rep_U_restraint->get_lst_rep_u());
+$persona_sans_chiffre->afficher_perso_etape_0();
+$persona_sans_chiffre->afficher_perso_etape_1();
+$persona_sans_chiffre->trier_decroissant();
+$persona_sans_chiffre->afficher_perso_etape_2();
+$persona_sans_chiffre->afficher_perso_etape_3();
+
+
+
  */
-
-$lst_rep_U_restraint->garder_que_elem($lst_rep_utilisateur->get_lst_rep_u(),$elementPrecis);
-    $lst_rep_U_restraint->toString();
-    //$lst_rep_pos->toString(); 
-    
-    $persona_sans_chiffre = new Persona();
-    $persona_pas_calculer = $persona_sans_chiffre->persona_creer($lst_rep_U_restraint->get_lst_rep_u());
-    $persona_sans_chiffre->afficher_perso_etape_0();
-    $persona_sans_chiffre->afficher_perso_etape_1();
-    $persona_sans_chiffre->trier_decroissant();
-    $persona_sans_chiffre->afficher_perso_etape_2();
-    $persona_sans_chiffre->afficher_perso_etape_3();
-
-
-/* 
-explication
-reponse questionnaire 1 fois
-faire 10~50 reponse random(orienter un truc ou des vraie reponse)
-environs 3 persona
-explication
-montrer que y a une coherence persona => reponse Utilisateur
-
-
-*/
-
-
-
-
 
 
 
