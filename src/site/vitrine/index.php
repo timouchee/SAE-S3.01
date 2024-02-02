b2dc628a5ade77a30787f0f43efab4d677fd5c7f
xan sallenave <xan.sallenave@gmail.com>
2024-02-02 14:19:46 +0100

    finition de la navbarr

--- src/site/vitrine/index.php
+++ src/site/vitrine/index.php
@@ -7,7 +7,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="path/to/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
     <title>BonPlan&Co</title>
 </head>
 <body class="p-3 m-0 border-0 bd-example m-0 border-0">
@@ -31,7 +34,7 @@
             <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="../newsletter/newslater.php">newsletter</a>
+            <a class="nav-link" href="../newsletter/newslater.php">Newsletter</a>
           </li>
           <li class="nav-item dropdown">
             <a
@@ -52,17 +55,19 @@
               <li><a class="dropdown-item" href="#">Offre d'emploie</a></li>
             </ul>
           </li>
+          <li><hr class="nav-divider" style= "color:white;"/></li>
           <li class="nav-item">
             <a class="nav-link disabled" aria-disabled="true">Map</a>
           </li>
           <li class="nav-item">
+            <a class="nav-link" href="#">Publier un Bon Plan</a>
+          </li>
+          <li><hr class="nav-divider" style= "color:white;"/></li>
+          <li class="nav-item">
             <a class="nav-link" href="#">Historique</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Publier un Bon Plan</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Param</i></a>
+            <a class="nav-link" href="#"><i class="icon-cog"> Paramètres</i></a>
           </li>
         </ul>
         
