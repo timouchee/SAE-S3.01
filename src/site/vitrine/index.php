183edb30e8d25484c03d534a3c6f8278264c6226
xan sallenave <xan.sallenave@gmail.com>
2024-03-05 15:13:55 +0100

    mise en place des liens pour page après connection

--- src/site/vitrine/index.php
+++ src/site/vitrine/index.php
@@ -14,66 +14,64 @@
     <title>BonPlan&Co</title>
 </head>
 <body class="p-3 m-0 border-0 bd-example m-0 border-0">
-  <nav class="navbar fixed-top " data-bs-theme="dark" style="background-color: rgba(0,2,3,255);">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src="titre.jpg" alt="img logo"></a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../newsletter/newslater.php">Newsletter</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <strong>BonPlan</strong>
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Activités</a></li>
-              <li><a class="dropdown-item" href="#">Evénements</a></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">Colocation</a></li>
-              <li><a class="dropdown-item" href="#">Covoiturage</a></li>
-              <li><a class="dropdown-item" href="#">Offre d'emploie</a></li>
+  <!--navbar + dropdown >> mettre en place if else avec verif connection-->
+  <nav class="navbar fixed-top" data-bs-theme="dark" style="background-color: rgba(0,2,3,255);">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="../vitrine/index.php"><img src="titre.jpg" alt="img logo"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style="background-color: rgba(0,2,3,255);">
+          <div class="offcanvas-header" style="flex-direction: row-reverse;">
+            <!-- <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><img src="titre.jpg" alt="img logo"></h5> -->
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div> 
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../newsletter/newslater.php">Newsletter</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <strong>BonPlan</strong>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Activités</a></li>
+                  <li><a class="dropdown-item" href="#">Evénements</a></li>
+                  <li><hr class="dropdown-divider" /></li>
+                  <li><a class="dropdown-item" href="#">Colocation</a></li>
+                  <li><a class="dropdown-item" href="#">Covoiturage</a></li>
+                  <li><a class="dropdown-item" href="#">Offre d'emploie</a></li>
+                </ul>
+              </li>
+              <li><hr class="nav-divider" style= "color:white;"/></li>
+              <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Map</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Publier un Bon Plan</a>
+              </li>
+              <li><hr class="nav-divider" style= "color:white;"/></li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Historique</a>
+              </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Paramètres</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-bs-toggle="modal" data-bs-target="#popUpConnection">Se connecter</a>
+                </li>
             </ul>
-          </li>
-          <li><hr class="nav-divider" style= "color:white;"/></li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Map</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Publier un Bon Plan</a>
-          </li>
-          <li><hr class="nav-divider" style= "color:white;"/></li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Historique</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#"><i class="icon-cog"> Paramètres</i></a>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+    <!-- /navbar + dropdown-->
 
+    <!--pop up explicative-->
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -86,23 +84,55 @@
         <p style="text-align: center;">Bon plan & co vous sert à trouver des bons plans à travers tout le BAB !</p>
         <p style="text-align: center;">Utilisez la barre de recherche et cherchez ce que vous voulez !
 
-Vous trouverez des activités, événement et des offres de covoiturage, colocation et d’offres d’emploi</p>
+        Vous trouverez des activités, événement et des offres de covoiturage, colocation et d’offres d’emploi</p>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">J'ai compris</button>
+          <button type="button" class="btn btn-primary" data-bs-dismiss="modal">J'ai compris</button>
         </div>
       </div>
     </div>
   </div>
+  <!--/pop up explicative-->
+
+  <!--pop up de connection >> à lier avec le bouton de connexion-->
+  <div class="modal fade" id="popUpConnection" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel"><strong>Connexion</strong></h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form action="script.php" method="post">  <!--lien vers le script de vérification-->
+          <label for="identifier">Identifiant de l'utilisateur:</label>
+          <br>
+          <input type="text" id="identifier" name="identifier" required>
+          <br>
+          <label for="password">Mot de passe:</label>
+          <br>
+          <input type="password" id="password" name="password" required>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary" >Se connecter</button>
+        </div>
+        <div>
+          <a href="../questionnaire/siteWebDemo_utilisateur.php">Première connexion ?</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /pop up de connection-->
 
 </body>
 
 <script>
-    document.addEventListener('DOMContentLoaded', function () {
-      var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
-      myModal.show();
-    });
-  </script>
+  //apparition auto pop up exmplicative
+  document.addEventListener('DOMContentLoaded', function () {
+    var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+    myModal.show();
+  });
+</script>
 
 </html>
