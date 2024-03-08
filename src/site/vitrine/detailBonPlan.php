950fc38cdeb27cea9ac71b119d09a1e9b39a195d
Virgile Espinasse <viviespi2002@gmail.com>
2024-03-08 09:15:14 +0100

    alignement de la page de detail

--- src/site/vitrine/detailBonPlan.php
+++ src/site/vitrine/detailBonPlan.php
@@ -5,16 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="path/to/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <title>Detail d'un bon plan</title>
 </head>
 <body>
 
     <?php
 
-    //include "header.php";
+    include "header.php";
 
 
     //Connexion BD
@@ -54,6 +58,8 @@
         
     //Partie code
 
+    echo "<div class='detailBonPlanContainer'>";
+
     echo "<h1>$libelleBonPlan</h1>";
     echo "<hr>";
 
@@ -66,13 +72,15 @@
     echo "</div>";
     echo "</div>";
 
-
+    echo "<br>";
 
     echo "<div class='card' style='width: 18rem;'>";
     echo "<div class='card-body'>";
     echo "<p class='card-title'>$detail</p>";
     echo "</div>";
     echo "</div>";
+
+    echo "<br>";
 
 
     echo "<div class='card' style='width: 18rem;'>";
@@ -82,11 +90,13 @@
     echo "</div>";
     echo "</div>";
 
+    echo "<br>";
+
     
 
     echo "<h1>Commentaires</h1>";
 
-    $queryCom="SELECT c.Message, c.note
+    $queryCom="SELECT c.Message, c.note, u.prenom, u.nom
     FROM BonPlan b JOIN Utilisateur u ON b.codeCarteEtudiante = u.codeCarteEtudiante
     JOIN Commentaire c ON b.idBonPlan = c.idBonPlan
     WHERE b.idBonPlan = 001"; // 001 a changer pour l'id du bonplan en question
@@ -94,19 +104,24 @@
 
     while ($data = mysqli_fetch_assoc($resultatCom))
     {
-      
+        $nom = $data["nom"];
+        $prenom = $data["prenom"];
         $message = $data["Message"];
         $note = $data["note"];
 
       //Partie code
-      echo "<div class='card' style='width: 18rem;'>";
+      echo "<p>$nom $prenom</p>";
+      echo "<div class='card' style='width: 25rem;'>";
     echo "<div class='card-body'>";
     echo "<p class='card-title'>$message</p>";
     echo "<p class='card-title'>note : $note</p>";
     echo "</div>";
     echo "</div>";
+    echo "<br>";
 
     }
+
+    echo "</div>";
 
     ?>
     
