8b98ddd137b0e79021dadbd21bde03da3e652cc8
xan sallenave <xan.sallenave@gmail.com>
2024-03-10 21:11:43 +0100

    ajout des formulaires pour les publications

--- src/site/vitrine/publier_event.php
+++ src/site/vitrine/publier_event.php
@@ -1,1 +1,15 @@
-publier_event
+<p id="title_up">Publier un événement</p>
+    
+<div id="barre_noire_fine_expand"></div>
+<br>
+<div id="lst_but">
+    <form action="publier_bon_plan.php" method="post">
+        <input type="image" src="" alt="Importer une photo">
+        <br><br>
+        <input type="text" name="ajout_description" id="ajout_description" value="Ajouter une description">
+        <br><br>
+        <input type="number" name="ajout_prix" id="ajout_prix" value="Ajouter un prix (optionnel)">
+        <br><br><br><br>
+        <button type="submit" class="but_admin_lst">Partager aux <br>administrateurs</button>
+    </form>
+</div>
